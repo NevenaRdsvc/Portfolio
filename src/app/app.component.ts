0b548144454cf71050c3020ef99b6083c6566464
Nevena <nevenaradosavljevic45@gmail.com>
2025-03-01 20:18:53 +0100

    Load event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { AfterViewInit, Component, NgZone, OnInit } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 import { register } from 'swiper/element/bundle';
 
 import { environment } from '../environments/environment';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LS_USER_LANGUAGE } from './shared/constants';
 import { SpinnerComponent } from './shared/components/spinner/spinner.component';
+import { LS_USER_LANGUAGE } from './shared/constants';
 
 @Component({
   selector: 'la-root',
@@ -16,13 +16,13 @@ import { SpinnerComponent } from './shared/components/spinner/spinner.component'
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements AfterViewInit {
   title(_title: any) {
     throw new Error('Method not implemented.');
   }
   showLoadingSpinner = true;
 
-  constructor(private translateService: TranslateService, private router: Router, private zone: NgZone) {
+  constructor(private translateService: TranslateService) {
     let languageToUse = environment.defaultLanguage;
     this.translateService.setDefaultLang(languageToUse);
     const savedLanguage = localStorage.getItem(LS_USER_LANGUAGE);
@@ -34,12 +34,7 @@ export class AppComponent implements OnInit, AfterViewInit {
     }
 
     this.translateService.use(languageToUse);
-  }
-
-  ngOnInit(): void {
-    this.zone.onStable.subscribe(() => {
-      this.showLoadingSpinner = false;
-    });
+    window.onload = () => this.showLoadingSpinner = false;
   }
 
   ngAfterViewInit(): void {
