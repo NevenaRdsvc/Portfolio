8f1d67d507e27be352ee996421fe2511f9fe15b9
Lazar <lazarst.pn@gail.com>
2025-05-27 22:59:07 +0200

    Latest

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,72 +1,15 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
-import { TranslatePipe, TranslateService } from '@ngx-translate/core';
-
-import Typewriter from 'typewriter-effect/dist/core';
+import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'la-contact',
-  standalone:true,
+  standalone: true,
   imports: [TranslatePipe],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent implements OnInit {
-  isIntersecting: boolean = false;
+export class ContactComponent {
+  constructor() { }
 
-  private typewriterWritedOnce: boolean = false;
-  private translatedText: string = '';
-  private observer: IntersectionObserver | null = null;
-
-  constructor(private host: ElementRef, private translate: TranslateService) {
-
-  }
-
-  ngOnInit(): void {
-    this.translatedText = this.translate.instant('labelMain.contact');
-    this.translate.onLangChange.subscribe(_ => { 
-      this.translatedText = this.translate.instant('labelMain.contact');
-
-      if (this.typewriterWritedOnce) {
-        this.initializeTypewriter();
-      }
-    });
-
-    const options = {
-      root: null,
-      rootMargin: '-200px',
-      threshold: 0
-    };
-
-    this.observer = new IntersectionObserver((entries, _) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          this.isIntersecting = true;
-          this.initializeTypewriter();
-
-          setTimeout(() => {
-            this.observer.unobserve(this.host.nativeElement);
-          }, 100);
-        }
-      });
-    }, options);
-
-    this.observer.observe(this.host.nativeElement);
-  }
-
-  private initializeTypewriter() {
-    const titleElement = document.getElementById('typewriter-title-contact');
-
-    if (titleElement) {
-      new Typewriter(titleElement, {
-        loop: false,
-        delay: 75,
-      })
-        .typeString(this.translatedText)
-        .pauseFor(1000)
-        .start();
-
-        this.typewriterWritedOnce = true;
-    }
-  }
 }
 
